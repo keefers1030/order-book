f28231107f6ce366eeade6072c05ec2bb6aeb3cb
Keith March Mistler <kmistler@gmail.com>
2021-04-22 14:56:16 -0400

    putting in same syntax for all formulas

--- orderBook.js
+++ orderBook.js
@@ -1,30 +1,43 @@
 /* eslint-disable no-console */
 const reconcileOrder = (existingBook, incomingOrder) => {
   if (existingBook.length === 0) {
-    existingBook.push((incomingOrder))
+    existingBook.push(incomingOrder)
 
     return existingBook
   }
   for (let i = 0; i < existingBook.length; i++) {
-    if (existingBook[i].type == incomingOrder.type ||
-      existingBook[i].type !== incomingOrder.type) {
+    if ((existingBook[i].type === incomingOrder.type ||
+      existingBook[i].type !== incomingOrder.type)) {
       existingBook.push(incomingOrder)
 
       return existingBook
     }
-    // fulfill order --- and remove duplicate order
-    else if ((existingBook[i].type !== incomingOrder.type) &&
-      (existingBook[i].quantity === incomingOrder.quantity)) {
+    // fulfill order --- and remove duplicate order of same quantity
+    else if ((existingBook[i].quantity === incomingOrder.quantity) &&
+      (existingBook[i].price === incomingOrder.price)) {
       existingBook.splice(i, 1)
 
       return existingBook
     }
+    // fulfills an order and reduces the matching order when the book contains a matching order of a larger quantity
+    else if ((existingBook[i].quantity > incomingOrder.quantity) &&
+      (existingBook[i].price === incomingOrder.price)) {
+      existingBook.splice(i, 1)
+
+      return existingBook
+    }
+    // partially fulfills an order, removes the matching order and adds the remainder of the order to the book 
+    // when the book contains a matching order of a smaller quantity
+    else if ((existingBook[i].quantity > incomingOrder.quantity) &&
+      (existingBook[i].price === incomingOrder.price)) {
+      existingBook.splice(i, 1)
+
+      return existingBook
+    }
+    return existingBook
+
   }
 
-  // fulfills an order and reduces the matching order when the book contains a matching order of a larger quantity
 
-
-}
-
-
-module.exports = reconcileOrder
+  console.log(reconcileOrder([{ type: 'buy', quantity: 10, price: 6150 }, { type: 'sell', quantity: 12, price: 6250 }], { type: 'sell', quantity: 10, price: 6150 }))
+  module.exports = reconcileOrder
