006f41b36e24e840aafea64c524dae459b3e757f
Keith March Mistler <kmistler@gmail.com>
2021-04-22 18:31:07 -0400

    partially fulfilled order

--- orderBook.js
+++ orderBook.js
@@ -22,17 +22,43 @@ const reconcileOrder = (existingBook, incomingOrder) => {
 
       return existingBook
     }
-    // needs to be last 
-    else if ((existingBook[i].type === incomingOrder.type ||
+    else if ((existingBook[i].type !== incomingOrder.type) &&
+      (existingBook[i].quantity < incomingOrder.quantity) &&
+      (existingBook[i].price === incomingOrder.price)) {
+      incomingOrder.quantity -= existingBook[i].quantity
+      existingBook.push(incomingOrder)
+      existingBook.shift()
+
+      return existingBook
+    }
+    else if ((existingBook[i].type !== incomingOrder.type) &&
+      (existingBook[i].quantity < incomingOrder.quantity) &&
+      (existingBook[i].price === incomingOrder.price)) {
+      incomingOrder.quantity -= existingBook[i].quantity
+      existingBook.splice(i, 2)
+      
+      console.table(existingBook)
+      return existingBook
+    }
+    // 2 existing orders to fulfill an order
+    // removes the first matching order and reduces the second
+    // else if ((existingBook[i].quantity === existingBook[i++].quantity) &&
+    //   (existingBook[i].type === existingBook[i++].type) &&
+    //   (existingBook[i].price === existingBook[i++].price)) {
+    //   console.log('nothing')
+
+    //   return existingBook
+    // }
+  }
+  for (let i = 0; i < existingBook.length; i++) {
+    if ((existingBook[i].type === incomingOrder.type ||
       existingBook[i].type !== incomingOrder.type)) {
       existingBook.push(incomingOrder)
 
       return existingBook
     }
-
-    return existingBook
   }
+
 }
 
-console.log(reconcileOrder([{ type: 'buy', quantity: 10, price: 6150 }, { type: 'sell', quantity: 12, price: 6250 }], { type: 'sell', quantity: 10, price: 6150 }))
 module.exports = reconcileOrder
