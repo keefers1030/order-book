c511e28557ce4f6d049502e9680a7a1b710a2607
Keith March Mistler <kmistler@gmail.com>
2021-04-22 13:53:26 -0400

    tests not fully working but code looks right

--- orderBook.js
+++ orderBook.js
@@ -5,34 +5,26 @@ const reconcileOrder = (existingBook, incomingOrder) => {
 
     return existingBook
   }
-
-
   for (let i = 0; i < existingBook.length; i++) {
-    if (existingBook[i].type === incomingOrder.type || existingBook[i].type !== incomingOrder.type) {
+    if (existingBook[i].type == incomingOrder.type ||
+      existingBook[i].type !== incomingOrder.type) {
       existingBook.push(incomingOrder)
 
       return existingBook
     }
-    // fulfill order(add order to beginning) --- and then remove duplicate order
-    if (incomingOrder.quantity === existingBook[i].quantity) {
-      existingBook.pop((incomingOrder))
-      existingBook.splice(existingBook[i])
-
-      return existingBook
-      console.log(existingBook)
-    }
-
-    // fulfills an order and reduces the matching order when the book contains a matching order of a larger quantity
-    if ((incomingOrder.type !== existingBook.type)
-      && (incomingOrder.quantity === existingBook.quantity)
-      && (incomingOrder.price === existingBook.price)) {
-      existingBook.push()
+    // fulfill order --- and remove duplicate order
+    else if ((existingBook[i].type !== incomingOrder.type) &&
+      (existingBook[i].quantity === incomingOrder.quantity)) {
+      existingBook.splice(i, 1)
 
       return existingBook
     }
   }
+
+  // fulfills an order and reduces the matching order when the book contains a matching order of a larger quantity
+
+
 }
-const existingBook = [{ type: 'sell', quantity: 10, price: 6150 }]
-const incomingOrder = { type: 'sell', quantity: 12, price: 6000 }
-reconcileOrder(existingBook, incomingOrder)
+
+
 module.exports = reconcileOrder
