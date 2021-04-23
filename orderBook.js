43c17258b885dc74cf267fe77dbbc657c91e2a89
Keith March Mistler <kmistler@gmail.com>
2021-04-22 20:12:02 -0400

    changed operand order

--- orderBook.js
+++ orderBook.js
@@ -6,57 +6,34 @@ const reconcileOrder = (existingBook, incomingOrder) => {
     return existingBook
   }
   for (let i = 0; i < existingBook.length; i++) {
-    if ((existingBook[i].type !== incomingOrder.type) &&
-      (existingBook[i].quantity === incomingOrder.quantity) &&
-      (existingBook[i].price === incomingOrder.price)) {
+    if ((incomingOrder.type !== existingBook[i].type) &&
+      (incomingOrder.quantity === existingBook[i].quantity) &&
+      (incomingOrder.price === existingBook[i].price)) {
       existingBook.splice(i, 1)
 
       return existingBook
     }
-    else if ((existingBook[i].type !== incomingOrder.type) &&
-      (existingBook[i].quantity > incomingOrder.quantity) &&
-      (existingBook[i].price === incomingOrder.price)) {
+    else if ((incomingOrder.type !== existingBook[i].type) &&
+      (incomingOrder.quantity < existingBook[i].quantity) &&
+      (incomingOrder.price === existingBook[i].price)) {
       existingBook[i].quantity -= incomingOrder.quantity
       existingBook.push(existingBook[i])
       existingBook.splice(i, 1)
 
       return existingBook
     }
-    else if ((existingBook[i].type !== incomingOrder.type) &&
-      (existingBook[i].quantity < incomingOrder.quantity) &&
-      (existingBook[i].price === incomingOrder.price)) {
+    else if ((incomingOrder.type !== existingBook[i].type) &&
+      (incomingOrder.quantity > existingBook[i].quantity) &&
+      (incomingOrder.price === existingBook[i].price)) {
       incomingOrder.quantity -= existingBook[i].quantity
-      existingBook.push(incomingOrder)
-      existingBook.shift()
-
-      return existingBook
+      existingBook.splice(i, 1)
     }
-    else if ((existingBook[i].type !== incomingOrder.type) &&
-      (existingBook[i].quantity < incomingOrder.quantity) &&
-      (existingBook[i].price === incomingOrder.price)) {
-      incomingOrder.quantity -= existingBook[i].quantity
-      existingBook.splice(i, 2)
-      
-      console.table(existingBook)
-      return existingBook
-    }
-    // 2 existing orders to fulfill an order
-    // removes the first matching order and reduces the second
-    // else if ((existingBook[i].quantity === existingBook[i++].quantity) &&
-    //   (existingBook[i].type === existingBook[i++].type) &&
-    //   (existingBook[i].price === existingBook[i++].price)) {
-    //   console.log('nothing')
-
-    //   return existingBook
-    // }
   }
-  for (let i = 0; i < existingBook.length; i++) {
-    if ((existingBook[i].type === incomingOrder.type ||
-      existingBook[i].type !== incomingOrder.type)) {
-      existingBook.push(incomingOrder)
+  if ((existingBook.type === incomingOrder.type ||
+    existingBook.type !== incomingOrder.type)) {
+    existingBook.push(incomingOrder)
 
-      return existingBook
-    }
+    return existingBook
   }
 
 }
